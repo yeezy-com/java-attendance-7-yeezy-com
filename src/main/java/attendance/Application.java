6ba487db71c76007cdf07951cd3630efc26cffea
GiSeok Kang <ime.rokz@gmail.com>
2024-12-14 14:06:10 +0900

    feat: csv 파일 읽기 기능 추가

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,7 +1,16 @@
 package attendance;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CsvFileReader csv = new CsvFileReader();
+        List<String> attendances = csv.read("attendances");
     }
 }
