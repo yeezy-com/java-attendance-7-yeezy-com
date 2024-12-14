bb361ac66f4d4d89e3e31758cc810fa7e3bfbdf6
GiSeok Kang <ime.rokz@gmail.com>
2024-12-14 16:13:18 +0900

    feat: InputView, OutputView 추가

--- src/main/java/attendance/config/AppConfig.java
+++ src/main/java/attendance/config/AppConfig.java
@@ -2,15 +2,33 @@ package attendance.config;
 
 import attendance.CsvFileReader;
 import attendance.controller.AttendanceController;
+import attendance.view.InputView;
+import attendance.view.OutputView;
 
 public class AppConfig {
 
     private CsvFileReader csvFileReader;
     private AttendanceController attendanceController;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public InputView inputView() {
+        if (inputView == null) {
+            this.inputView = new InputView();
+        }
+        return inputView;
+    }
+
+    public OutputView outputView() {
+        if (outputView == null) {
+            this.outputView = new OutputView();
+        }
+        return outputView;
+    }
 
     public AttendanceController attendanceController() {
         if (attendanceController == null) {
-            this.attendanceController = new AttendanceController();
+            this.attendanceController = new AttendanceController(inputView(), outputView());
         }
         return attendanceController;
     }
---
+++ src/main/java/attendance/view/InputView.java
@@ -0,0 +1,10 @@
+package attendance.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputChoice() {
+        return Console.readLine();
+    }
+}
